2441ff63215e4da5f533624007c8ad8c2596706a
hasibul-99 <hasibul_uap@outlook.com>
2024-04-26 01:17:24 +0600

    auth navbar design implement

--- components/shared/AuthNavbar.js
+++ components/shared/AuthNavbar.js
@@ -2,41 +2,46 @@ import React from 'react'
 import { Select, Space } from 'antd';
 import Image from 'next/image';
 
+// https://codepen.io/Blockshot/pen/rNwOYBE
 export default function AuthNavbar() {
     return (
         <header>
             <nav className="flex flex-wrap items-center justify-between w-full py-4 md:py-0 px-4 text-lg text-gray-700 bg-white">
                 <div>
                     <a href="#">
-                    <Image width={200} height={60} src="/assets/images/logo.png" />
+                        <Image width={200} height={60} src="/assets/images/logo.png" />
                     </a>
                 </div>
+                <a href="#">
+                    <svg xmlns="<http://www.w3.org/2000/svg>" id="menu-button" class="h-6 w-6 cursor-pointer md:hidden block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </a>
                 <div className="hidden w-full md:flex md:items-center md:w-auto " id="menu">
-                    <ul className="text-base text-gray-700 pt-4 md:flex md:justify-between md:pt-0">
+                    <ul className="text-base text-gray-700 pt-4 md:mt-3 md:flex md:justify-between items-center md:pt-0">
                         <li>
-                            <a className="md:p-4 py-2 block hover:text-green-400" href="#">Home</a>
+                            <a className="md:px-5 py-4 block hover:text-green-400" href="#">Home</a>
                         </li>
                         <li>
-                            <a className="md:p-4 py-2 block hover:text-green-400" href="#">Packages</a>
+                            <a className="md:px-5 py-4 block hover:text-green-400" href="#">Packages</a>
                         </li>
                         <li>
-                            <a className="md:p-4 py-2 block hover:text-green-400" href="#">News</a>
+                            <a className="md:px-5 py-4 block hover:text-green-400" href="#">News</a>
                         </li>
                         <li>
-                            <a className="md:p-4 py-2 block hover:text-green-400" href="#">Blog</a>
+                            <a className="md:px-5 py-4 block hover:text-green-400" href="#">Blog</a>
                         </li>
                         <li>
-                            <a className="md:p-4 py-2 block hover:text-green-400" href="#">FAQ</a>
+                            <a className="md:px-5 py-4 block hover:text-green-400" href="#">FAQ</a>
                         </li>
                         <li>
-                            <a className="md:p-4 py-2 block hover:text-green-400" href="#">About Us</a>
+                            <a className="md:px-5 py-4 block hover:text-green-400" href="#">About Us</a>
                         </li>
                         <li>
-                            <a className="md:p-4 py-2 block hover:text-green-400" href="#">Contact Us</a>
+                            <a className="md:px-5 py-4 block hover:text-green-400" href="#">Contact Us</a>
                         </li>
                         <li>
                             <Select
-                                className='mt-3'
                                 defaultValue="en"
                                 style={{
                                     width: 120,
@@ -45,17 +50,26 @@ export default function AuthNavbar() {
                                 options={[
                                     {
                                         value: 'en',
-                                        label: 'English',
+                                        label: <Space>
+                                            <img src="/assets/images/united-kingdom.png" />
+                                            English
+                                        </Space>,
                                     },
                                     {
                                         value: 'bn',
-                                        label: 'বাংলা',
+                                        label: <Space>
+                                            <img src="/assets/images/bangladesh.png" />
+                                            বাংলা
+                                        </Space>,
                                     }
                                 ]}
                             />
                         </li>
                         <li>
-                            <a className="md:p-4 py-2 block hover:text-green-400" href="#">Log In</a>
+                            <a className="md:px-5 py-4 block hover:text-green-400" href="#">Log In</a>
+                        </li>
+                        <li>
+                            <a class="btn-hover color-5 md:px-12 py-4" href="#">Sign up</a>
                         </li>
                     </ul>
                 </div>
