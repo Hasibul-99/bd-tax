fb13cf436c51de9e980d1685ffa7c2df81aa9944
hasibul-99 <hasibul_uap@outlook.com>
2024-05-07 22:02:07 +0600

    use client implement

--- app/[locale]/(root)/docs/page.js
+++ app/[locale]/(root)/docs/page.js
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import { ConfigProvider, Button, Space } from 'antd';
 import { RightOutlined } from '@ant-design/icons';
---
+++ app/[locale]/(root)/premium-plus/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PremiumPlus() {
+  return (
+    <div>PremiumPlus</div>
+  )
+}
