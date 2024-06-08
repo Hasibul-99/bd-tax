5fa6e54f5fee2a3bc7d3e612ab1ca917f2f95d08
hasibul-99 <hasibul_uap@outlook.com>
2024-06-08 21:41:19 +0600

    update content

--- components/shared/MoreLeftNavebar.js
+++ components/shared/MoreLeftNavebar.js
@@ -1,20 +1,28 @@
 
 import { moreNaveData } from '@/scripts/helper';
 import { List, Space } from 'antd';
-import Link from 'next/link';
-import { useState } from 'react';
 import Cookies from "js-cookie";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 
 
-export default function MoreLeftNavebar({locale}) {
+export default function MoreLeftNavebar({ locale }) {
+  const pathname = usePathname()
   const [selected, setSelected] = useState(1);
 
+  console.log("pathname", pathname);
+
   const handelChange = (name) => {
-    console.log();
     if (name === 'Logout') {
       Cookies.remove("bdtax_token");
       Cookies.remove("bdtax_user");
     }
+  }
+
+  const isActiveContent = (url) => {
+    if (pathname.startsWith(url)) return true;
+    else return false;
   }
 
   return (
@@ -23,9 +31,9 @@ export default function MoreLeftNavebar({locale}) {
         className='mt-5'
         size="large"
         dataSource={moreNaveData}
-        renderItem={(item) => <List.Item className={selected === item.id ? 'bg-[#E2ECE5] rounded-md !text-[#126A25]' : '' + item.id == 9 ? ' !bg-[#FFEBEA] rounded-md !text-[#BA040A]' : ''}
+        renderItem={(item) => <List.Item className={isActiveContent(`/${locale}` + item.url) ? 'bg-[#E2ECE5] rounded-md !text-[#126A25]' : '' + item.id == 9 ? ' !bg-[#FFEBEA] rounded-md !text-[#BA040A]' : ''}
           style={{ 'border-block-end': 0 }}>
-          <Link href={`/${locale}` + item.url} onClick={() => {handelChange(item.name)}}>
+          <Link href={`/${locale}` + item.url} onClick={() => { handelChange(item.name) }}>
             <Space>
               <img src={item.icon} alt={item.name} />
               {item.name}
