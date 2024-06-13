098094c53e1d36d62a838007141c82e4280d6645
Hasibul-125 <hasibul.hasan@asthait.com>
2024-06-13 12:22:58 +0600

    update

---
+++ app/[locale]/loading.js
@@ -0,0 +1,16 @@
+export default function Loading() {
+  return (
+    <div className='text-center h-dvh flex justify-items-center items-center relative'>
+      <div>
+        <img
+          className='image'
+          src='/assets/icons/loading.svg'
+          alt='Premium Plus'
+        />
+      </div>
+      <div className='absolute inset-x-11'>
+        <p>Establishing secure connection. </p>
+      </div>
+    </div>
+  )
+}
