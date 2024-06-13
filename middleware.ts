50f11d6ae68c709cd8e0bbf22e9306e94a65d232
Hasibul-125 <hasibul.hasan@asthait.com>
2024-06-13 21:29:30 +0600

    update

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,19 @@
-import createMiddleware from 'next-intl/middleware';
- 
+import createMiddleware from 'next-intl/middleware'
+
 export default createMiddleware({
   // A list of all locales that are supported
   locales: ['en', 'bn'],
- 
+
   // Used when no locale matches
-  defaultLocale: 'en'
-});
- 
+  defaultLocale: 'en',
+})
+
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(bn|en)/:path*']
-};
+  // matcher: ['/', '/(bn|en)/:path*']
+
+  // Match only internationalized pathnames
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|apple-touch-icon.png|favicon.svg|images/books|icons|manifest|assets|js).*)',
+  ],
+}
