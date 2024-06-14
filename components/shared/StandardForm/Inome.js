bbd124cbab345a6f6f752aea00084a3ddebef9e1
Hasibul-125 <hasibul.hasan@asthait.com>
2024-06-14 20:59:09 +0600

    update

--- components/shared/StandardForm/Inome.js
+++ components/shared/StandardForm/Inome.js
@@ -1,6 +1,16 @@
 import {GET_incomeAssetLiabilySourceList} from '@/scripts/api'
 import {postData} from '@/scripts/api-service'
-import {Checkbox, Col, ConfigProvider, Divider, Row, Typography} from 'antd'
+import {RightOutlined} from '@ant-design/icons'
+import {
+  Button,
+  Checkbox,
+  Col,
+  ConfigProvider,
+  Divider,
+  Row,
+  Space,
+  Typography,
+} from 'antd'
 import {Fragment, useEffect, useState} from 'react'
 const {Title, Text} = Typography
 
@@ -22,114 +32,144 @@ export default function Inome() {
   }, [])
 
   return (
-    <div className='md:mt-8 md:h-[80vh] md:overflow-y-scroll scrollbar'>
-      <ConfigProvider
-        theme={{
-          token: {
-            colorPrimary: '#4B7F52',
-          },
-          components: {
-            Button: {
+    <div>
+      <div className='md:mt-8 md:h-[80vh] md:overflow-y-scroll scrollbar'>
+        <ConfigProvider
+          theme={{
+            token: {
               colorPrimary: '#4B7F52',
             },
-          },
-        }}
-      >
-        <div>
-          <Title level={5}>Your income sources</Title>
-          <Text>
-            Please select the source of your income from the options below
-          </Text>
-          <Checkbox.Group
-            className='my-5 w-full'
-            // onChange={onChange}
-          >
-            <Row>
-              {contentData?.income?.length ? (
-                <>
-                  {contentData.income.map((item) => (
-                    <Fragment key={item.id}>
-                      <Col span={24}>
-                        <Checkbox value={item.id}>{item.title}</Checkbox>
-                      </Col>
-                      <Divider className='my-3' />
-                    </Fragment>
-                  ))}
-                </>
-              ) : null}
-            </Row>
-          </Checkbox.Group>
-        </div>
+            components: {
+              Button: {
+                colorPrimary: '#4B7F52',
+              },
+            },
+          }}
+        >
+          <div>
+            <Title level={5}>Your income sources</Title>
+            <Text>
+              Please select the source of your income from the options below
+            </Text>
+            <Checkbox.Group
+              className='my-5 w-full'
+              // onChange={onChange}
+            >
+              <Row>
+                {contentData?.income?.length ? (
+                  <>
+                    {contentData.income.map((item) => (
+                      <Fragment key={item.id}>
+                        <Col span={24}>
+                          <Checkbox value={item.id}>{item.title}</Checkbox>
+                        </Col>
+                        <Divider className='my-3' />
+                      </Fragment>
+                    ))}
+                  </>
+                ) : null}
+              </Row>
+            </Checkbox.Group>
+          </div>
 
-        <div>
-          <Title level={5}>Your assets</Title>
-          <Checkbox.Group
-            className='my-5 w-full'
-            // onChange={onChange}
-          >
-            <Row>
-              {contentData?.assets?.length ? (
-                <>
-                  {contentData.assets.map((item) => (
-                    <Fragment key={item.id}>
-                      <Col span={24}>
-                        <Checkbox value={item.id}>{item.title}</Checkbox>
-                      </Col>
-                      <Divider className='my-3' />
-                    </Fragment>
-                  ))}
-                </>
-              ) : null}
-            </Row>
-          </Checkbox.Group>
-        </div>
+          <div>
+            <Title level={5}>Your assets</Title>
+            <Checkbox.Group
+              className='my-5 w-full'
+              // onChange={onChange}
+            >
+              <Row>
+                {contentData?.assets?.length ? (
+                  <>
+                    {contentData.assets.map((item) => (
+                      <Fragment key={item.id}>
+                        <Col span={24}>
+                          <Checkbox value={item.id}>{item.title}</Checkbox>
+                        </Col>
+                        <Divider className='my-3' />
+                      </Fragment>
+                    ))}
+                  </>
+                ) : null}
+              </Row>
+            </Checkbox.Group>
+          </div>
 
-        <div>
-          <Title level={5}>Your Expence</Title>
-          <Checkbox.Group
-            className='my-5 w-full'
-            // onChange={onChange}
-          >
-            <Row>
-              {contentData?.expence?.length ? (
-                <>
-                  {contentData.expence.map((item) => (
-                    <Fragment key={item.id}>
-                      <Col span={24}>
-                        <Checkbox value={item.id}>{item.title}</Checkbox>
-                      </Col>
-                      <Divider className='my-3' />
-                    </Fragment>
-                  ))}
-                </>
-              ) : null}
-            </Row>
-          </Checkbox.Group>
-        </div>
+          <div>
+            <Title level={5}>Your Expence</Title>
+            <Checkbox.Group
+              className='my-5 w-full'
+              // onChange={onChange}
+            >
+              <Row>
+                {contentData?.expence?.length ? (
+                  <>
+                    {contentData.expence.map((item) => (
+                      <Fragment key={item.id}>
+                        <Col span={24}>
+                          <Checkbox value={item.id}>{item.title}</Checkbox>
+                        </Col>
+                        <Divider className='my-3' />
+                      </Fragment>
+                    ))}
+                  </>
+                ) : null}
+              </Row>
+            </Checkbox.Group>
+          </div>
 
-        <div>
-          <Title level={5}>Your Libility</Title>
-          <Checkbox.Group
-            className='my-5 w-full'
-            // onChange={onChange}
+          <div>
+            <Title level={5}>Your Libility</Title>
+            <Checkbox.Group
+              className='my-5 w-full'
+              // onChange={onChange}
+            >
+              <Row>
+                {contentData?.libility?.length ? (
+                  <>
+                    {contentData.libility.map((item) => (
+                      <Fragment key={item.id}>
+                        <Col span={24}>
+                          <Checkbox value={item.id}>{item.title}</Checkbox>
+                        </Col>
+                        <Divider className='my-3' />
+                      </Fragment>
+                    ))}
+                  </>
+                ) : null}
+              </Row>
+            </Checkbox.Group>
+          </div>
+        </ConfigProvider>
+      </div>
+
+      <div className='text-center'>
+        <ConfigProvider
+          className='text-center'
+          theme={{
+            token: {
+              colorPrimary: '#126A25',
+            },
+            components: {
+              Button: {
+                colorPrimary: '#126A25',
+              },
+            },
+          }}
+        >
+          <Button
+            type='primary'
+            htmlType='submit'
+            className='btn-hover color-5 !h-11 !rounded-xl px-10 mt-5 m-auto'
+            size='large'
           >
-            <Row>
-              {contentData?.libility?.length ? (
-                <>
-                  {contentData.libility.map((item) => (
-                    <Fragment key={item.id}>
-                      <Col span={24}>
-                        <Checkbox value={item.id}>{item.title}</Checkbox>
-                      </Col>
-                      <Divider className='my-3' />
-                    </Fragment>
-                  ))}
-                </>
-              ) : null}
-            </Row>
-          </Checkbox.Group>
-        </div>
-      </ConfigProvider>
+            <Space>
+              Next
+              <RightOutlined style={{fontSize: '12px', marginTop: '8px'}} />
+            </Space>
+          </Button>
+        </ConfigProvider>
+      </div>
     </div>
   )
 }
