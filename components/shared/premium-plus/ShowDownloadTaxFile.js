7f494e88b7b5fed30dd5a9f909849553ab8d60f9
Hasibul-125 <hasibul.hasan@asthait.com>
2024-06-23 08:57:37 +0600

    update

--- components/shared/premium-plus/ShowDownloadTaxFile.js
+++ components/shared/premium-plus/ShowDownloadTaxFile.js
@@ -1,66 +1,86 @@
-import { GET_DOWNLOAD_TAX_FILE, SAVE_CONSENT_DOC } from '@/scripts/api';
-import { getData, postData } from '@/scripts/api-service';
-import { Typography, Checkbox, ConfigProvider, Button  } from 'antd';
-import { useEffect, useRef, useState } from 'react';
-const { Title } = Typography;
+import {GET_DOWNLOAD_TAX_FILE, SAVE_CONSENT_DOC} from '@/scripts/api'
+import {getData, postData} from '@/scripts/api-service'
+import {Button, Checkbox, ConfigProvider, Typography} from 'antd'
+import {useEffect, useState} from 'react'
+const {Title} = Typography
 
-export default function ShowDownloadTaxFile({ setShow }) {
-    const [fileData, setFileData] = useState();
-    const [isConfirm, setIsConfirm] = useState();
+export default function ShowDownloadTaxFile({setShow}) {
+  const [fileData, setFileData] = useState()
+  const [isConfirm, setIsConfirm] = useState()
 
-    const getDownloadTaxFile = async () => {
-        let res = await getData(GET_DOWNLOAD_TAX_FILE);
+  const getDownloadTaxFile = async () => {
+    let res = await getData(GET_DOWNLOAD_TAX_FILE)
 
-        if (res) {
-            console.log(res);
-            setFileData(res?.data)
-        }
+    if (res) {
+      setFileData(res?.data)
     }
+  }
 
-    const handelSubmit = async() => {
-        let res = await postData(SAVE_CONSENT_DOC);
+  const handelSubmit = async () => {
+    let res = await postData(SAVE_CONSENT_DOC)
 
-        if (res) {
-            setShow(2)
-        }
+    if (res) {
+      setShow(2)
     }
- 
-    useEffect(() => {
-        getDownloadTaxFile()
-    }, [])
+  }
 
-    return (
-        <div>
-            <Title level={4}>Please review your Tax return form</Title>
+  useEffect(() => {
+    getDownloadTaxFile()
+  }, [])
 
-            {
-                fileData?.pdf_file_path ? <>
-                    <object data={fileData?.pdf_file_path} type="application/pdf" width="100%" height="500vh">
-                        <p><a href={fileData?.pdf_file_path}></a></p>
-                    </object>
-                </> : ''
-            }
+  return (
+    <div>
+      <Title level={4}>Please review your Tax return form</Title>
 
+      {fileData?.pdf_file_path ? (
+        <>
+          <object
+            data={fileData?.pdf_file_path}
+            type='application/pdf'
+            width='100%'
+            height='500vh'
+          >
+            <p>
+              <a href={fileData?.pdf_file_path}></a>
+            </p>
+          </object>
+        </>
+      ) : (
+        ''
+      )}
 
-            <div className='text-center mt-8'>
-                <ConfigProvider
-                    theme={{
-                        token: {
-                            colorPrimary: "#4B7F52",
-                        },
-                        components: {
-                            Button: {
-                                colorPrimary: "#4B7F52",
-                            },
-                        },
-                    }}
-                >
-                    <Checkbox onChange={(e) => {setIsConfirm(e.target.checked)}}>I certify all docs provided by me is accurate.</Checkbox>
-                        <Button type="primary" disabled={!isConfirm} className='px-10 mt-5 flex m-auto' onClick={() => { handelSubmit() }}>
-                        Approve & Sign
-                        </Button>
-                </ConfigProvider>
-            </div>
-        </div>
-    )
+      <div className='text-center mt-8'>
+        <ConfigProvider
+          theme={{
+            token: {
+              colorPrimary: '#4B7F52',
+            },
+            components: {
+              Button: {
+                colorPrimary: '#4B7F52',
+              },
+            },
+          }}
+        >
+          <Checkbox
+            onChange={(e) => {
+              setIsConfirm(e.target.checked)
+            }}
+          >
+            I certify all docs provided by me is accurate.
+          </Checkbox>
+          <Button
+            type='primary'
+            disabled={!isConfirm}
+            className='prime-button w-52 m-auto mt-4'
+            onClick={() => {
+              handelSubmit()
+            }}
+          >
+            Approve & Sign
+          </Button>
+        </ConfigProvider>
+      </div>
+    </div>
+  )
 }
--- components/shared/premium-plus/Submit.js
+++ components/shared/premium-plus/Submit.js
@@ -1,27 +1,24 @@
-import { GET_DOWNLOAD_TAX_FILE } from '@/scripts/api';
-import { getData } from '@/scripts/api-service';
-import { useEffect, useRef, useState } from 'react';
-import SignatureUpload from './SignatureUpload';
-import ShowDownloadTaxFile from './ShowDownloadTaxFile';
-import OrderStatus from './OrderStatus';
-
+import {useRef, useState} from 'react'
+import OrderStatus from './OrderStatus'
+import ShowDownloadTaxFile from './ShowDownloadTaxFile'
+import SignatureUpload from './SignatureUpload'
 
 export default function Submit() {
-  const ref = useRef(null);
+  const ref = useRef(null)
   const [show, setShow] = useState(1)
 
-
   return (
-    <div ref={ref} className='mt-12' >
-      {
-        show === 1 ? <ShowDownloadTaxFile setShow={setShow}/> : ''
-      }
-      {
-        show === 2 ? <SignatureUpload offsetWidth={ref?.current?.offsetWidth || 0} setShow={setShow}/> : ''
-      }
-      {
-        show === 3 ? <OrderStatus showNextButton={false}/> : ''
-      }
+    <div ref={ref} className='mt-12 p-10'>
+      {show === 1 ? <ShowDownloadTaxFile setShow={setShow} /> : ''}
+      {show === 2 ? (
+        <SignatureUpload
+          offsetWidth={ref?.current?.offsetWidth || 0}
+          setShow={setShow}
+        />
+      ) : (
+        ''
+      )}
+      {show === 3 ? <OrderStatus showNextButton={false} /> : ''}
     </div>
   )
 }
