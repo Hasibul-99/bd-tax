fce08b74b02522ce74bc33337a3499d9fe090e86
Hasibul-125 <hasibul.hasan@asthait.com>
2024-07-08 00:16:12 +0600

    update

---
+++ app/[locale]/(public)/validatebkashInvoice/page.js
@@ -0,0 +1,54 @@
+'use client'
+
+import {Button, Result} from 'antd'
+import Link from 'next/link'
+import {useSearchParams} from 'next/navigation'
+
+export default function validatebkashInvoice() {
+  const searchParams = useSearchParams()
+  const status = searchParams.get('status')
+
+  return (
+    <div>
+      {status === 'success' ? (
+        <>
+          <Result
+            status='success'
+            title='Order Successfully Completed!'
+            subTitle='Thank you for your purchase. configuration takes few minutes, please wait.'
+            extra={[
+              <Link href={'/'}>
+                <Button
+                  className='prime-button w-52 !mx-auto'
+                  type='primary'
+                  key='console'
+                >
+                  Go Console
+                </Button>
+              </Link>,
+            ]}
+          />
+        </>
+      ) : (
+        <>
+          <Result
+            status='error'
+            title='Submission Failed'
+            subTitle="We're sorry, but there was a problem processing your order. Please try again later or contact support for assistance."
+            extra={
+              <Link href={'/'}>
+                <Button
+                  className='prime-button w-52 !mx-auto'
+                  type='primary'
+                  key='console'
+                >
+                  Go Console
+                </Button>
+              </Link>
+            }
+          />
+        </>
+      )}
+    </div>
+  )
+}
