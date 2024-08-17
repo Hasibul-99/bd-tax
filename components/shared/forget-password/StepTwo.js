e0d9853dd6720f20bdbb92872f369ef26fe0a1b9
Hasibul-125 <hasibul.hasan@asthait.com>
2024-08-17 11:22:42 +0600

    forget password condition update

--- components/shared/forget-password/StepTwo.js
+++ components/shared/forget-password/StepTwo.js
@@ -1,24 +1,38 @@
 import {SUBMIT_PASSWORD} from '@/scripts/api'
 import {postData} from '@/scripts/api-service'
+import {alertPop} from '@/scripts/helper'
 import {Button, Form, Input} from 'antd'
 import {useRouter} from 'next/navigation'
+import {useState} from 'react'
 
 export default function StepTwo({stepOneRes}) {
   const [form] = Form.useForm()
   const router = useRouter()
+  const [loading, setLoading] = useState(false)
 
   const onFinish = async (values) => {
+    setLoading(true)
     let data = {
       password_token: stepOneRes?.token,
       sixdigit_code: values.sixdigit_code,
       password: values.password,
       c_password: values.c_password,
     }
-    const res = await postData(SUBMIT_PASSWORD, data)
+    const res = await postData(SUBMIT_PASSWORD, data, 'no_token', 'showError')
 
+    // if (res) {
+    //   let masterData = res?.data?.data
+    //   router.push('signin')
+    // }
     if (res) {
-      let masterData = res?.data?.data
-      router.push('signin')
+      if (res.code === 'error') {
+        form.setFields(res?.errors)
+      } else {
+        alertPop('success', res.message)
+        router.push('signin')
+      }
+
+      setLoading(false)
     }
   }
 
@@ -68,7 +82,12 @@ export default function StepTwo({stepOneRes}) {
       </Form.Item>
 
       <Form.Item>
-        <Button className='prime-button' type='primary' htmlType='submit'>
+        <Button
+          className='prime-button'
+          type='primary'
+          htmlType='submit'
+          loading={loading}
+        >
           Submit Password
         </Button>
       </Form.Item>
