4ed5402afd6f078cc488d7e242d927345188e4a1
Hasibul-125 <hasibul.hasan@asthait.com>
2024-09-17 01:19:03 +0600

    update

--- components/shared/Footer.js
+++ components/shared/Footer.js
@@ -27,9 +27,11 @@ export default function Footer() {
         </div>
 
         <div className='text-center sm:flex sm:justify-between sm:text-left'>
-          <p className='text-sm'>
-            <span className='block sm:inline'>
+          <p className='text-sm text-center'>
+            <span className=' block sm:inline'>
               © 2024 bdtax.com.bd All Rights Reserved.
+              <br />
+              DK Tower, 94 Bir Uttam CR Datta Road, Banglamotor, Dhaka-1205
             </span>
           </p>
           <p className='mt-4 text-sm sm:order-first sm:mt-0'>
